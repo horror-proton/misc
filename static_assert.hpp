5b5810368560351185fc74766646b135400e035e
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2022-06-15 13:54:57 +0800

    Add static_assert

---
+++ static_assert.hpp
@@ -0,0 +1,19 @@
+//! Provide static assertion to delete instantiation of template with message. (`static_assert(false)` does not work)
+
+#pragma once
+
+namespace misc::detail {
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCUnusedTemplateParameterInspection"
+template<bool Cond, typename ...Ts>
+struct bool_constant_at_template_instantiation {
+    static constexpr bool value = Cond;
+};
+#pragma clang diagnostic pop
+
+}
+
+/// does not work for those without template parameter, use =delete or requires.
+#define misc_static_assert_false(Message, ...) \
+static_assert(misc::detail::bool_constant_at_template_instantiation<false, __VA_ARGS__>::value, Message)
