7746dfd7cbfc1d5217e96bae775692f22940710f
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2022-07-04 00:20:20 +0800

    Use rvalue reference in tuple_for_each

--- tuple_for_each.hpp
+++ tuple_for_each.hpp
@@ -12,15 +12,17 @@ using std::size_t;
 namespace detail {
 
 template<class TupleT, typename Func, typename ...Args, std::size_t ...I>
-constexpr auto tuple_for_each_impl(std::index_sequence<I...>, TupleT &t, Func, Args...args) {
-    (Func{}(get<I>(t), args...), ...); // TODO: what to return
+constexpr auto tuple_for_each_impl(std::index_sequence<I...>, TupleT &t, Func, Args &&...args) {
+    (Func{}(get<I>(t), std::forward<Args>(args)...), ...); // TODO: what to return
 }
 
 }
 
 template<class TupleT, typename Func, typename ...Args>
-constexpr auto tuple_for_each(TupleT &t, Func /* [](auto &e, Args...args) { ... } */, Args...args) {
-    detail::tuple_for_each_impl(std::make_index_sequence<std::tuple_size_v<TupleT>>{}, t, Func{}, args...);
+constexpr auto tuple_for_each(TupleT &t, Func /* [](auto &e, Args...args) { ... } */, Args &&...args) {
+    detail::tuple_for_each_impl(
+            std::make_index_sequence<std::tuple_size_v<TupleT>>{},
+            t, Func{}, std::forward<Args>(args)...);
 }
 
 }
