0d0c8dcee2569e2c7c6d56b3e8dfd1818c8504fc
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2022-07-03 19:46:19 +0800

    Add supports for other arguments in tuple_for_each

--- tuple_for_each.hpp
+++ tuple_for_each.hpp
@@ -11,16 +11,16 @@ using std::size_t;
 
 namespace detail {
 
-template<class TupleT, typename Func, size_t ...I>
-constexpr auto tuple_for_each_impl(TupleT &t, Func, std::index_sequence<I...>) {
-    (Func{}(get<I>(t)), ...); // TODO: what to return
+template<class TupleT, typename Func, typename ...Args, std::size_t ...I>
+constexpr auto tuple_for_each_impl(std::index_sequence<I...>, TupleT &t, Func, Args...args) {
+    (Func{}(get<I>(t), args...), ...); // TODO: what to return
 }
 
 }
 
-template<class TupleT, typename Func>
-constexpr auto tuple_for_each(TupleT &t, Func /* [](auto &e) { ... } */) {
-    detail::tuple_for_each_impl(t, Func{}, std::make_index_sequence<std::tuple_size_v<TupleT>>{});
+template<class TupleT, typename Func, typename ...Args>
+constexpr auto tuple_for_each(TupleT &t, Func /* [](auto &e, Args...args) { ... } */, Args...args) {
+    detail::tuple_for_each_impl(std::make_index_sequence<std::tuple_size_v<TupleT>>{}, t, Func{}, args...);
 }
 
 }
